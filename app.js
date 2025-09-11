ccbc7a1a5b5f0b236fb61204030a0e9e5d5df852
tajwararik <tajwar.arik.10@gmail.com>
2025-09-11 12:44:21 +0600

    Added middleware for instrumentsRoutes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("node:path");
+const instrumentsRoutes = require("./routes/instrumentsRoutes");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,9 +10,13 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
+
+app.use("/instruments", instrumentsRoutes);
 
 app.listen(PORT, (error) => {
   if (error) throw error;
