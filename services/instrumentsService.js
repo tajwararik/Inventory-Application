4df2dd20db13b3a6c48f2ea2cfa37c10f218b7f5
tajwararik <tajwar.arik.10@gmail.com>
2025-09-13 19:14:19 +0600

    Created functions for querying data

--- services/instrumentsService.js
+++ services/instrumentsService.js
@@ -12,6 +12,59 @@ async function listOfInstruments() {
   return rows;
 }
 
+async function getInstrumentDetails(id) {
+  const result = await pool.query(
+    `SELECT i.*, c.name AS category_name, b.name AS brand_name
+     FROM instruments i
+     JOIN categories c ON i.category_id = c.id
+     JOIN brands b ON i.brand_id = b.id
+     WHERE i.id = $1`,
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+async function createInstrument(
+  name,
+  category_id,
+  brand_id,
+  serial_number,
+  condition,
+  quantity,
+  price
+) {
+  await pool.query(
+    `INSERT INTO instruments (name, category_id, brand_id, serial_number,condition, quantity, price) 
+     VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+    [name, category_id, brand_id, serial_number, condition, quantity, price]
+  );
+}
+
+async function deleteInstrument(id) {
+  await pool.query(`DELETE FROM instruments WHERE id=$1`, [id]);
+}
+
+async function updateInstrument(
+  id,
+  name,
+  category_id,
+  brand_id,
+  serial_number,
+  condition,
+  quantity,
+  price
+) {
+  await pool.query(
+    `UPDATE instruments SET name=$1, category_id=$2, brand_id=$3, serial_number=$4, condition=$5, quantity=$6, price=$7 WHERE id=$8`,
+    [name, category_id, brand_id, serial_number, condition, quantity, price, id]
+  );
+}
+
 module.exports = {
   listOfInstruments,
+  getInstrumentDetails,
+  createInstrument,
+  deleteInstrument,
+  updateInstrument,
 };
